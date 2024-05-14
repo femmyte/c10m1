e43a9b656fab301334eb5f1ab7e519725278456b
femmyte <sanyaoluadefemi@gmail.com>
2024-05-14 17:13:38 +0100

    Added script file to the webpage

---
+++ script.js
@@ -0,0 +1,10 @@
+// alert("hey you! welcome to todays ILS session");
+
+console.log("hello world!");
+
+let num1 = 10;
+let num2 = 20;
+let total = num1 + num2;
+
+div = document.getElementById("result");
+div.innerHTML = total;
